0b84fb8ee7f7e741b34421272f1efe6e510fb763
jj fantini <jjfantini@gmail.com>
2023-02-24 10:11:33 -0500

    DONE: discontinuity visualization in R

--- breakpoints.R
+++ breakpoints.R
@@ -1,52 +1,52 @@
-#Install required packages
+# Step 1: Install required packages
 install.packages("strucchange")
 
-# Load the required packages
+# 1.1 Load the required packages
 library(tidyquant)
 library(strucchange)
 
-# Retrieve the last 5 years of data for AMD
+# Step 2: Retrieve the last 5 years of data for AMD
 amd <- tq_get("AMD", from = Sys.Date() - years(5), to = Sys.Date())
 
-# Extract the adjusted close prices as a time series
+# 2.1 Extract the adjusted close prices as a time series
 amd_ts <- ts(amd$adjusted, frequency = 1)
 
-# Perform the test
+# Step 3: Perform the breakpoitns test test
 bp_test <- breakpoints(amd_ts ~ 1)
 
-# Extract the estimated breakpoints
+# 3.1 Extract the estimated breakpoints
 bp <- bp_test$breakpoints
 
-# Calculate mean values between breakpoints
-means <- c()
-for (i in 1:(length(bp)-1)){
-        start <- bp[i] + 1
-        end <- bp[i+1]
-        means[i] <- mean(amd_ts[start:end])
-}
 
-#calculate last mean until end of time series
-means[length(bp)] <- mean(amd_ts[(bp[length(bp)]+1):length(amd_ts)])
+# Step 4:  Calculate means of each breakpoint segment
+# 4.1 Calculate the first mean
+means <- c(mean(amd_ts[1:bp[1]]))
 
-# Plot time series with vertical lines at breakpoints and horizontal lines at mean values
-plot(amd_ts, main = "AMD Adjusted Close Prices", xaxt = "n")
-
-abline(v = bp, col = "blue")
-
-for (i in 1:length(bp)){
-        if (i == 1){
-                lines(x = seq(1, bp[i]), y = rep(means[i], bp[i]), col = "red", lty = 2, xlim = c(1, bp[i]))
-        } else if (i != length(bp)) {
-                lines(x = seq(bp[i], bp[i+1]-1), y = rep(means[i], bp[i+1]-bp[i]), col = "red", lty = 2, xlim = c(bp[i], bp[i+1]))
+# 4.2 Calculate the rest of the means between breakpoints
+for (i in 1:length(bp)) {
+        if (i == length(bp)) {
+                means <- c(means, mean(amd_ts[(bp[i] + 1):length(amd_ts)]))
         } else {
-                lines(x = seq(bp[i], length(amd_ts)), y = rep(means[i], length(amd_ts)-bp[i]+1), col = "red", lty = 2, xlim = c(bp[i], length(amd_ts)))
+                means <- c(means, mean(amd_ts[(bp[i] + 1):bp[i + 1]]))
         }
 }
 
-for (i in 1:length(bp)){
-        if (i != length(bp)) {
-                lines(x = seq(bp[i], bp[i+1]-1), y = rep(means[i], bp[i+1]-bp[i]), col = "red", lty = 2, xlim = c(bp[i], bp[i+1]))
+
+
+# Step 5: Plot time series with vertical lines at breakpoints and horizontal lines at mean values
+# 5.1 Plot the original time series
+plot(amd_ts, main = "AMD Adjusted Close Prices", xaxt = "n")
+
+# 5.2 Plot the breakpoints
+abline(v = bp, col = "blue")
+
+# Plot means between breakpoints
+for (i in 1:length(means)) {
+        if (i == 1) {
+                lines(c(1, bp[i]), c(means[i], means[i]), col = "red", lty = 2)
+        } else if (i == length(means)) {
+                lines(c(bp[i - 1] + 1, length(amd_ts)), c(means[i], means[i]), col = "red", lty = 2)
         } else {
-                lines(x = seq(bp[i], length(amd_ts)), y = rep(means[i], length(amd_ts)-bp[i]+1), col = "red", lty = 2, xlim = c(bp[i], length(amd_ts)))
+                lines(c(bp[i - 1] + 1, bp[i]), c(means[i], means[i]), col = "red", lty = 2)
         }
 }

478252e2aee54aab10a132fb355256d6676b8f81
jj fantini <jjfantini@gmail.com>
2023-03-04 07:05:12 -0500

    DONE: liquidity_Py

--- Py/liquidity.py
+++ Py/liquidity.py
@@ -4,63 +4,75 @@
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
+import yfinance as yf
 import matplotlib.pyplot as plt
 from bidask import edge
 
 # Step 2: Collect past 5 years of price data for SNAP and save it to an object named 'snap_df'
-from_date = datetime.now() - timedelta(days=5*365)
-to_date = datetime.now()
-snap_df = pd.DataFrame(tq.get('SNAP', from_date, to_date))
+snap_df = yf.download('SNAP', start='2016-03-03', end='2023-03-03')
 
 # Step 2.1: Plot and visualize SNAP price
-plt.plot(snap_df['date'], snap_df['close'])
+plt.plot(snap_df['Close'])
 plt.xlabel('Date')
 plt.ylabel('Price')
 plt.title('SNAP Price')
 plt.show()
 
 # Step 3: Convert snap_df to xts object - we only want OHLC
-snap_xts = pd.DataFrame(snap_df[['open', 'high', 'low', 'close']].values,
-                        index=pd.DatetimeIndex(snap_df['date']), columns=['open', 'high', 'low', 'close'])
+snap_ohlc = pd.DataFrame(snap_df[['Open', 'High', 'Low', 'Close']].values,
+                        index=snap_df.index, columns=['Open', 'High', 'Low', 'Close'])
 
-# Step 4: Estimate bid-ask spread using spread() function -- 1month rolling liquidity calc
+                        
+
+# Step 4: Estimate bid-ask spread using edge() function -- 1 month rolling liquidity calc
 # 4.1 Calculate 1m rolling spread estimation
-snap_edge = edge(snap_xts)
 
-# 4.2 convert xts to data frame for easy plotting
-snap_spread_df = pd.DataFrame({'Date': snap_spread.index, 'spread': snap_spread['EDGE']})
+###############################
+def calculate_edge(data):
+    """
+    Calculates bid-ask spread using edge function on a rolling 21-day window of data.
 
-# Step 5: Create a data frame to store both spread calculations
+    Parameters:
+        data (pd.DataFrame): OHLC price data.
 
-# 5.1 Find different in df length (snap_spread_df is 21 rows shorter due to rolling window)
-row_diff = len(snap_df) - len(snap_spread_df)
+    Returns:
+        pd.Series: Edge values for each row of input data.
+    """
+    # Create an empty series to hold the edge values
+    edge_values = pd.Series(index=data.index, dtype = "float64")
 
-# 5.2 Create df with both time series you'd like to plot
-plot_df = pd.DataFrame({'date': snap_spread_df['Date'], 
-                        'price': snap_df['close'][row_diff:], 
-                        'spread.edge': snap_spread_df['spread'].values*100,
-                        'spread.gmm': snap_spread['GMM'].values*100,
-                        'spread.ar': snap_spread['AR'].values*100})
+    # Calculate edge function on a rolling 21-day window
+    for i in range(21, len(data)):
+        window_data = data.iloc[i-21:i]
+        edge_values.iloc[i] = edge(window_data['Open'], window_data['High'], window_data['Low'], window_data['Close'])
 
-# Step 6: Visualize liquidity and price for SNAP
-# 6.1 Create y-axis scaling factor
-scaleFactor_edge = plot_df['price'].max() / plot_df['spread.edge'].max()
-scaleFactor_gmm = plot_df['price'].max() / plot_df['spread.gmm'].max()
-scaleFactor_ar = plot_df['price'].max() / plot_df['spread.ar'].max()
+ # Remove the first 21 rows of edge_values
+    edge_values = edge_values[21:]
+    
+    return edge_values
+###############################
 
-# 6.2 create plot for edge
+snap_spread = calculate_edge(snap_ohlc) * 100
+
+# Step 5: Plot
+# Create a figure and axis object
 fig, ax1 = plt.subplots()
-ax1.plot(plot_df['date'], plot_df['price'], color='blue', linewidth=1.5)
+
+# Plot snap_df price on primary y-axis
+ax1.plot(snap_df.index, snap_df['Close'], color='blue')
 ax1.set_xlabel('Date')
 ax1.set_ylabel('Price', color='blue')
 ax1.tick_params(axis='y', labelcolor='blue')
+
+# Create a secondary y-axis
 ax2 = ax1.twinx()
-ax2.plot(plot_df['date'], plot_df['spread.edge']*scaleFactor_edge, color='red')
+
+# Plot snap_edge_rolling on secondary y-axis
+ax2.plot(snap_spread.index, snap_spread.values, color='red')
 ax2.set_ylabel('Spread (%)', color='red')
 ax2.tick_params(axis='y', labelcolor='red')
-ax2.axhline(y=scaleFactor_edge, color='black', linestyle='dashed', linewidth=1.2, label='1% Spread Max')
-ax2.legend(loc='upper right')
 plt.title('$SNAP Bid-Ask EDGE Spread Estimation vs Price (1m rolling spread)')
-plt.show()
 
-# 6.3 create plot for
+
+# Show the plot
+plt.show()

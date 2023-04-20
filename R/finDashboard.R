ed3dccd4fc4b7d52dcd40db64bde2e32caca144e
jj fantini <jjfantini@gmail.com>
2023-04-20 14:17:14 -0400

    ADDED: finDashboard.R

---
+++ R/finDashboard.R
@@ -0,0 +1,96 @@
+# 1.Prerequisites and Installing Packages
+install.packages("shiny")
+install.packages("tidyquant")
+install.packages("TTR")
+install.packages("shinythemes")
+
+library(shiny)
+library(tidyquant)
+library(TTR)
+library(shinythemes)
+
+# 2. Building the UI for the Shiny App
+ui <- fluidPage(
+        titlePanel("Market Data Analytics App"),
+        # Create layout with sidebar and main panel
+        sidebarLayout(
+                sidebarPanel(
+                        selectInput("ticker", "Select Ticker:", choices = c("AAPL", "AMD", "GOOG", "AMZN", "TSLA")),
+                        selectInput("date_length", "Select Date Length:", choices = c("1y", "2y", "3y", "4y", "5y")),
+                        selectInput("analysis_method", "Select Analysis Method:", choices = c("ATR", "Rolling Momentum", "Relative Strength Index")),
+                        actionButton("calculate", "Calculate"),
+                        themeSelector()
+                ),
+                # Display plots in main panel
+                mainPanel(
+                        plotOutput("price_plot"),
+                        plotOutput("analysis_output"),
+                        class = "custom-main-panel"
+                )
+        ),
+        # Custom CSS for main panel positioning
+        tags$style(HTML("
+            .custom-main-panel {
+                margin-top: -60px; /* Adjust this value to move the mainPanel up or down */
+            }
+        "))
+)
+
+
+# 3. Implementing Analysis Techniques
+server <- function(input, output) {
+        # Apply selected theme to plots
+        thematic::thematic_shiny()
+
+        # Fetch stock data on button click
+        stock_data <- eventReactive(input$calculate, {
+                date_range <-  switch(input$date_length,
+                                      "1y" = 365,
+                                      "2y" = 2 * 365,
+                                      "3y" = 3 * 365,
+                                      "4y" = 4 * 365,
+                                      "5y" = 5 * 365)
+                # Collect data from tidyquant
+                tq_get(input$ticker, from = Sys.Date() - days(date_range), to = Sys.Date()) %>%
+                        dplyr::select(date, close, high, low)
+        })
+
+
+        # Store selected analysis method on button click
+        selected_analysis <- reactiveValues(method = NULL)
+
+        # Update selected_analysis on button click
+        observeEvent(input$calculate, {
+                selected_analysis$method <- input$analysis_method
+        })
+
+        # Render stock price plot
+        output$price_plot <- renderPlot({
+                req(stock_data()) # Ensure the data is available before plotting
+                selected_data <- stock_data()
+                plot(selected_data$date, selected_data$close, type = "l", main = "Stock Price", xlab = "Date", ylab = "Price")
+        })
+
+        # Render analysis plot based on method
+        output$analysis_output <- renderPlot({
+                req(stock_data(), selected_analysis$method) # Ensure the data and analysis method are available before plotting
+                selected_data <- stock_data()
+                selected_xts <- xts::xts(selected_data[, -1], order.by = selected_data$date)
+
+                # Analysis code logic
+                if (selected_analysis$method == "ATR") {
+                        atr <- ATR(HLC = selected_xts[, c("high", "low", "close")], n = 14)
+                        plot(index(selected_xts), atr$atr, type = "l", main = "Average True Range", xlab = "Date", ylab = "ATR")
+                }  else if (selected_analysis$method == "Rolling Momentum") {
+                        rolling_momentum <- lag(selected_xts$close, 12) / selected_xts$close - 1
+                        plot(index(selected_xts), rolling_momentum, type = "l", main = "Rolling Momentum", xlab = "Date", ylab = "Momentum")
+
+                } else if (selected_analysis$method == "Relative Strength Index") {
+                        rsi <- RSI(selected_xts$close, n = 14, maType = "SMA")
+                        plot(index(selected_xts), rsi, type = "l", main = "Relative Strength Index", xlab = "Date", ylab = "RSI")
+                }
+        })
+}
+
+# 5. Run the app
+shinyApp(ui = ui, server = server)
